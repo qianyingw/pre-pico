85ba5568891513ba14a2dc530197aff0694b86f7
Qianying Wang <w.qianying@outlook.com>
2020-12-22 13:25:53 +0000

    crf option

--- conll/predict.py
+++ conll/predict.py
@@ -7,8 +7,11 @@ Created on Mon Dec 14 12:54:11 2020
 """
 
 import tensorflow as tf
+import tensorflow_addons as tfa
 import spacy
 nlp = spacy.load('en_core_sci_sm')
+
+from model import BiLSTM, CRF, BiLSTM_CRF
 
 #%% Predict
 
@@ -30,10 +33,19 @@ def pred_one_text(text, word2idx, idx2tag, model):
         
     seqs = tf.convert_to_tensor(seqs, dtype=tf.int32)  # [seq_len]
     seqs = tf.expand_dims(seqs, 0)  # [batch_size=1, seq_len]
-    
-    logits = model(seqs)  # [1, seq_len, num_tags]
-    probs = tf.nn.softmax(logits, axis=2)  # [1, seq_len, num_tags]
-    preds = tf.argmax(probs, axis=2)  # [1, seq_len]
+       
+    if isinstance(model, BiLSTM):   
+        logits = model(seqs)  # [1, seq_len, num_tags] 
+        probs = tf.nn.softmax(logits, axis=2)  # [1, seq_len, num_tags]
+        preds = tf.argmax(probs, axis=2)  # [1, seq_len]
+        
+    if isinstance(model, (CRF, BiLSTM_CRF)):
+        logits, _ = model(seqs)  # [1, seq_len, num_tags] 
+        logit = tf.squeeze(logits, axis = 0)  # [seq_len, num_tags]
+        viterbi, _ = tfa.text.viterbi_decode(logit, model.trans_pars)  # [seq_len]      
+        # viterbi, _ = tfa.text.viterbi_decode(logit[:text_len], model.transition_params)  # [text_len]
+        preds = tf.convert_to_tensor(viterbi, dtype = tf.int32)  # [seq_len]  
+        preds = tf.expand_dims(preds, 0)  # [1, seq_len]
 
     tags = [idx2tag[idx] for idx in preds.numpy()[0].tolist()]
     
