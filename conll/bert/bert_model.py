2e5358764de903c8e78167e7ad4a70736049ba17
Qianying Wang <w.qianying@outlook.com>
2020-12-30 19:57:15 +0000

    bert-lstm-crf

--- conll/bert/bert_model.py
+++ conll/bert/bert_model.py
@@ -11,6 +11,7 @@ from transformers import DistilBertPreTrainedModel, DistilBertConfig, DistilBert
 
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from torchcrf import CRF
 
 #%%
@@ -46,6 +47,59 @@ class BERT_CRF(BertPreTrainedModel):
             mask_cut.append(mask[tag!=-100])
             tags_cut.append(tag[tag!=-100])
         
+        probs_cut = torch.stack(probs_cut)  # [batch_size, seq_len-2]
+        mask_cut = torch.stack(mask_cut)   # [batch_size, seq_len-2]
+        tags_cut = torch.stack(tags_cut)  # [batch_size, seq_len-2]
+                     
+              
+        if labels is None:
+            preds = self.crf.decode(probs_cut, mask=mask_cut)  # assign mask for 'unpad'
+            return preds  # preds: list of list containing best tag seqs for each batch
+        else:
+            # log_likelihood = self.crf(probs_cut, tags_cut, mask=mask_cut)  # [batch_size]
+            log_likelihood = self.crf(F.softmax(probs_cut, dim=2), tags_cut, mask=mask_cut)  # [batch_size]
+            preds = self.crf.decode(probs_cut, mask=mask_cut)  # assign mask for 'unpad'
+            return preds, log_likelihood
+
+#%%
+class BERT_LSTM_CRF(BertPreTrainedModel):
+    
+    def __init__(self, config):
+        super().__init__(config)
+        
+        self.config = config
+        self.bert = BertModel(config)
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+        
+        self.lstm = nn.LSTM(input_size = config.hidden_size, hidden_size = config.hidden_size,
+                            num_layers = 1, dropout = config.hidden_dropout_prob, 
+                            batch_first = True, bidirectional = False)
+        
+        self.fc = nn.Linear(config.hidden_size, config.num_labels)
+        
+        self.init_weights()
+        self.crf = CRF(config.num_labels, batch_first=True)
+        
+     
+    def forward(self, input_ids, attention_mask, labels=None):
+               
+        outputs = self.bert(input_ids, attention_mask)
+        hidden_states = outputs[0]  # [batch_size, seq_len, hidden_size]
+        
+        out_dp = self.dropout(hidden_states)
+        out_lstm, (h_n, c_n) = self.lstm(out_dp)    # [batch_size, seq_len, hidden_size]         
+        emission_probs = self.fc(out_lstm)  # [batch_size, seq_len, num_tags]
+        
+        attention_mask = attention_mask.type(torch.uint8)
+        
+        probs_cut, mask_cut, tags_cut = [], [], []
+        for prob, mask, tag in zip(emission_probs, attention_mask, labels):
+            # prob: [seq_len, num_tags]
+            # mask/tag: [seq_len]
+            probs_cut.append(prob[tag!=-100, :])
+            mask_cut.append(mask[tag!=-100])
+            tags_cut.append(tag[tag!=-100])
+        
         probs_cut = torch.stack(probs_cut) 
         mask_cut = torch.stack(mask_cut) 
         tags_cut = torch.stack(tags_cut) 
@@ -58,7 +112,8 @@ class BERT_CRF(BertPreTrainedModel):
             log_likelihood = self.crf(probs_cut, tags_cut, mask=mask_cut)  # [batch_size]
             preds = self.crf.decode(probs_cut, mask=mask_cut)  # assign mask for 'unpad'
             return preds, log_likelihood
-
+        
+        
 #%%
 class Distil_CRF(DistilBertPreTrainedModel):
     
