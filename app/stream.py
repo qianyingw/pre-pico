f3f7858b0187433d67b2bcfbf364f2a12d9b0a17
Qianying Wang <w.qianying@outlook.com>
2021-05-10 23:04:32 +0100

    ent err

--- app/stream.py
+++ app/stream.py
@@ -9,7 +9,7 @@ Created on Fri Apr  9 19:18:10 2021
 import json
 import pubmed_parser
 import pred_fn
-from pred_fn import sent_detect, pred_one_bert, tup2dict
+from pred_fn import sent_detect, pico_extract, tup2dict
 
 import streamlit as st
 # streamlit run /home/qwang/pre-pico/app/stream.py
@@ -23,8 +23,9 @@ prfs_path = '/home/qwang/pre-pico/app/b0_bio.json'
 pth_path = '/home/qwang/pre-pico/app/b0_bio_last.pth.tar' 
 
 #%% App
-st.header('PICO recognition for in vivo abstract')
-pmid = st.text_input('Input one PMID: ', 23326526)
+st.header('PICO extraction for in vivo abstract')
+# pmid = st.text_input('Input one PMID: ', 23326526)
+pmid = st.text_input('Input one PMID: ', 27231887)
 
 
 try:
@@ -34,8 +35,8 @@ try:
     if text == "":
         st.text("No abstract available!")
     else:      
-        st.write(title)
-        st.write(text)
+        # st.write(title)
+        # st.write(text)
         
         ## Extract pico text   
         text = sent_detect(text, sent_pth)
@@ -44,22 +45,28 @@ try:
             dat = json.load(f)    
         idx2tag = dat['idx2tag']
         # Extract pico phrases              
-        tup = pred_one_bert(text, mod, pre_wgts, pth_path, idx2tag)
+        tup = pico_extract(text, mod, pre_wgts, pth_path, idx2tag)
         res = tup2dict(tup)
         
-        st.write("""### PICO:  ###
+        st.write("""### Extracted PICO text  ###
                  """) 
-        if res['Species']:
+        st.write(text)
+        
+        keys = [k for k, v in res.items()]
+        
+        st.write("""### Extracted PICO phrases  ###
+                 """) 
+        if 'Species' in keys:
             st.write("""Species: """, res['Species'])
-        if res['Strain']:
+        if 'Strain' in keys:
             st.write("""Strain: """, res['Strain'])
-        if res['Induction']:
+        if 'Induction' in keys:
             st.write("""Induction: """, res['Induction'])
-        if res['Intervention']:
+        if 'Intervention' in keys:
             st.write("""Intervention: """, res['Intervention'])
-        if res['Comparator']:
+        if 'Comparator' in keys:
             st.write("""Comparator: """, res['Comparator'])
-        if res['Outcome']:
+        if 'Outcome' in keys:
             st.write("""Outcome: """, res['Outcome'])
         
 except:
